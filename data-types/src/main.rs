f3d8d3084fb648e3e7e24a708dd8be6d50e6e59c
mmednik <mmednik@gmail.com>
2022-11-06 16:30:53 -0300

    enums

--- data-types/src/main.rs
+++ data-types/src/main.rs
@@ -8,6 +8,17 @@ struct Student {
 // tuple struct
 struct Grades(char, char, char, char, f32);
 
+#[derive(Debug)]
+struct KeyPress(String, char);
+#[derive(Debug)]
+struct MouseClick {x:i64, y:i64}
+#[derive(Debug)]
+enum WebEvent {
+    WELoad(bool),
+    WEKeys(KeyPress),
+    WEClick(MouseClick)
+}
+
 fn main() {
     let number:u32 = 14;
     println!("The number is {}.", number);
@@ -45,4 +56,15 @@ fn main() {
     
     println!("{}, level {}. Remote: {}. Grades: {}, {}, {}, {}. Average: {}.",
         user_2.name, user_2.level, user_2.remote, mark_2.0, mark_2.1, mark_2.2, mark_2.3, mark_2.4);
+
+    let we_load = WebEvent::WELoad(true);
+    let click = MouseClick {x:100, y:250};
+    println!("Mouse click loaction: {}, {}", click.x, click.y);
+
+    let we_click = WebEvent::WEClick(click);
+    let keys = KeyPress(String::from("Ctrl+"), 'N');
+    println!("\nKeys pressed: {}{}", keys.0, keys.1);
+
+    let we_key = WebEvent::WEKeys(keys);
+    println!("\nWebEvent enum structure: \n\n {:#?} \n\n {:#?} \n\n {:#?}", we_load, we_click, we_key);
 }
