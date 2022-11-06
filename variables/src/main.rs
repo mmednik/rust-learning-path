4ed2775b1279ca7dac8e766805943dc961040dc8
mmednik <mmednik@gmail.com>
2022-11-06 15:33:31 -0300

    variable shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -5,4 +5,9 @@ fn main() {
     a_number = 15;
     println!("The number is {}.", a_number);
     println!("The word is {}.", a_word);
+
+    let shadow_num = 5;
+    let shadow_num = shadow_num + 5;
+    let shadow_num = shadow_num * 2;
+    println!("The number is {}.", shadow_num);
 }
