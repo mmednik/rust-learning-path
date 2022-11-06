1de3618a9281495dc96e43b99b1e7a415c4e2872
mmednik <mmednik@gmail.com>
2022-11-06 16:46:09 -0300

    return a value

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,9 +2,16 @@ fn goodbye(message: &str) {
     println!("{}", message);
 }
 
+fn divide_by_5(num: u32) -> u32 {
+    num / 5
+}
+
 fn main() {
     let formal = "Good bye.";
     let casual = "See you later!";
     goodbye(formal);
     goodbye(casual);
+
+    let num = 25;
+    println!("{} divided by 5 = {}", num, divide_by_5(num));
 }
